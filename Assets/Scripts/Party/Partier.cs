12de4f34c093a0e3797621b99b245d5b3fd34e83
bigredwill <bigredwill@gmail.com>
2016-01-30 12:17:29 -0800

    Moved party stuff to Party folder

--- Assets/Scripts/Party/Partier.cs
+++ Assets/Scripts/Party/Partier.cs
@@ -4,10 +4,19 @@ using System.Collections;
 public class Partier : MonoBehaviour {
 
 	Animator animator;
+	public float facePlayerTime = 2.0f;
+	public float faceAwayTime = 5.0f;
+
+	public bool facing;
+
+	private lastTurnTime;
+
+
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		lastTurn = Time.time;
 	}
 	
 	// Update is called once per frame
