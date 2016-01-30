8fe8949d5ef3c18ffe68cc220745f89fc0162486
bigredwill <bigredwill@gmail.com>
2016-01-30 14:56:53 -0800

    party clown

--- Assets/Scripts/Party/Partier.cs
+++ Assets/Scripts/Party/Partier.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Partier : MonoBehaviour {
 
 	Animator animator;
-	public float facePlayerTime = 2.0f;
-	public float faceAwayTime = 5.0f;
+	public float facePlayerTime = 2.0f; //The time (in secs) Partier faces the screen.
+	public float faceAwayTime = 5.0f; //The time (in secs) Partier faces away from the screen.
 
-	public bool facing;
+	public bool facing = false;
 
 	private float lastTurnTime;
 
@@ -17,22 +17,34 @@ public class Partier : MonoBehaviour {
 	void Start () {
 		animator = GetComponent<Animator> ();
 		lastTurnTime = Time.time;
+
+		//set next faceAwayTime to a random val
+		faceAwayTime = Random.Range(2,6);
+
+		//set next facePlayerTime to a random val
+		facePlayerTime = Random.Range(0.5f,4);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		float timePassed = Time.time - lastTurnTime;
 
 		if (!facing) {
+			if (timePassed > faceAwayTime) {
+				animator.SetTrigger("FacePlayer");
+				facing = true;
+				lastTurnTime = Time.time;
+			}
+		} else {
+			if (timePassed > facePlayerTime) {
+				animator.SetTrigger("UnFacePlayer");
+				facing = false;
+				lastTurnTime = Time.time;
 
+			}
 		}
-
-
-		bool fire = Input.GetButtonDown("Fire1");
-		if (fire == true) {
-			animator.SetTrigger("FacePlayer");
-			Debug.Log ("hello");
-		}
-	
 	}
+
+	
 }
