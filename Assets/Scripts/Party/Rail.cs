73b587de2bd1bbab07b1aa5b3bc3f60be0fa9664
Henry Tran <hen@Henrys-MacBook-Pro.local>
2016-01-31 13:31:21 -0800

    working party

--- Assets/Scripts/Party/Rail.cs
+++ Assets/Scripts/Party/Rail.cs
@@ -7,21 +7,25 @@ public class Rail : MonoBehaviour {
 	public GameObject[] rounds;
 
 
-	public int groupDistanceFromCamera = 22;
-	public int numRounds = 3;
+//	public int groupDistanceFromCamera = 22;
+//	public int numRounds = 3;
 
-	private int round = 0;
-	private string choice;
+//	private int round = 0;
+//	private string choice;
 	private Round currentRound;
 
-	private bool ended = false;
-	private bool checking = false;
+//	private bool ended = false;
+//	private bool checking = false;
+
+	private bool failedCheck = false;
+
+	public string lastSide;
 
 	public float cameraAngle = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-		currentRound = rounds [round++].GetComponent<Round> ();
+//		currentRound = rounds [round++].GetComponent<Round> ();
 	}
 
 	
@@ -32,16 +36,20 @@ public class Rail : MonoBehaviour {
 
 		float goalYaw = 0.0f;
 
-		if (input > 0.2f) {
-			choice = "right";
-			goalYaw = 15.7777f;
-		} else if (input < -0.2f) {
-			choice = "left";
-			goalYaw = -15.7777f;
+		if (failedCheck) {
+			if (lastSide == "right") {
+				goalYaw = 15.7777f;
+			} else if (lastSide == "left") {
+				goalYaw = -15.7777f;
+			}
 		} else {
-			choice = "none";
+			if (input > 0.2f) {
+				goalYaw = 15.7777f;
+			} else if (input < -0.2f) {
+				goalYaw = -15.7777f;
+			}
 		}
-		currentRound.select (choice);
+//		currentRound.select (choice);
 
 		cameraAngle += (goalYaw - cameraAngle) * 0.15f;
 		Quaternion angles = transform.localRotation;
@@ -50,54 +58,86 @@ public class Rail : MonoBehaviour {
 		angles.eulerAngles = eulerAngle;
 		transform.localRotation = angles;
 
-		if (ended) {
-			ended = false;
+//		if (ended) {
+//			ended = false;
 //			Global.LoadLevel ("PartyRail");
-			Global.LoadLevel ("Handshake");
-		}
+//			Global.LoadLevel ("Handshake");
+//		}
 
-		if (checking) {
-			check ();
-		}
+//		if (checking) {
+//			check ();
+//		}
 
 	}
 
 	public void check() {
+		float input = Input.GetAxis ("Horizontal");
 
-		bool success = currentRound.check (choice);
-
-		if (success == true) {
-			Global.AddPoints (5f);
-		} else {
-			if (choice == "none")
-				Global.SubtractPoints (10);
-			else {
+		if (input > 0.1f) {
+			if (lastSide == "right") {
 				Global.SubtractPoints (5);
-				ended = true;
-				Global.LoadLevel ("Handshake");
+				failedCheck = true;
+				currentRound.fail (lastSide);
+			} else {
+				Global.AddPoints (5f);
+				currentRound.cool ("right");
 			}
-
+		} else if (input < -0.1f) {
+			if (lastSide == "left") {
+				Global.SubtractPoints (5);
+				failedCheck = true;
+				currentRound.fail (lastSide);
+			} else {
+				Global.AddPoints (5f);
+				currentRound.cool ("left");
+			}
+		} else {
+			Global.SubtractPoints (10);
+			failedCheck = true;
+			currentRound.fail (lastSide);
 		}
+
+//		bool success = currentRound.check (choice);
+//
+//		if (success == true) {
+//			Global.AddPoints (5f);
+//		} else {
+//			if (choice == "none")
+//				Global.SubtractPoints (10);
+//			else {
+//				Global.SubtractPoints (5);
+//				ended = true;
+//				Global.LoadLevel ("Handshake");
+//			}
+//
+//		}
 			
 	}
 
 	public void startCheck() {
-		checking = true;
+//		checking = true;
 	}
 
 	public void endCheck() {
-		currentRound = rounds [round++].GetComponent<Round> ();
-		checking = false;
+//		currentRound = rounds [round++].GetComponent<Round> ();
+//		checking = false;
+		if (failedCheck) {
+			Global.LoadLevel ("Handshake");
+		}
 
 	}
 
-	public void activate() {
-		currentRound.activate ();
+	public void activate(int round) {
+		lastSide = Random.Range (0, 2) == 1 ? "right" : "left";
+		currentRound = rounds [round].GetComponent<Round> ();
+		currentRound.activate (lastSide);
+//		currentRound.activate ();
 	}
 
 	public void end() {
-		ended = true;
-		Debug.Log ("end");
+		Global.LoadLevel ("Handshake");
+//		ended = true;
+//		Debug.Log ("end");
 	}
 	
 }
--- Assets/Scripts/Party/Round.cs
+++ Assets/Scripts/Party/Round.cs
@@ -13,12 +13,12 @@ public class Round : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		side = Random.Range (0, 2) == 1 ? "right" : "left";
-		if (side == "right") {
-			partyGroup = rightGroup.GetComponent<PartyGroup> ();
-		} else {
-			partyGroup = leftGroup.GetComponent<PartyGroup> ();
-		}
+//		side = Random.Range (0, 2) == 1 ? "right" : "left";
+//		if (side == "right") {
+//			partyGroup = rightGroup.GetComponent<PartyGroup> ();
+//		} else {
+//			partyGroup = leftGroup.GetComponent<PartyGroup> ();
+//		}
 	}
 	
 	// Update is called once per frame
@@ -27,44 +27,65 @@ public class Round : MonoBehaviour {
 	}
 
 	public void select(string direction) {
-		if (direction == "right") {
-			rightGroup.GetComponent<PartyGroup> ().highlight(Color.cyan);
-			leftGroup.GetComponent<PartyGroup> ().highlight(Color.white);
-		} else if (direction == "left") {
-			leftGroup.GetComponent<PartyGroup> ().highlight(Color.cyan);
-			rightGroup.GetComponent<PartyGroup> ().highlight(Color.white);
-		} else {
-
-			leftGroup.GetComponent<PartyGroup> ().highlight(Color.white);
-			rightGroup.GetComponent<PartyGroup> ().highlight(Color.white);
-		}
+//		if (direction == "right") {
+//			rightGroup.GetComponent<PartyGroup> ().highlight(Color.cyan);
+//			leftGroup.GetComponent<PartyGroup> ().highlight(Color.white);
+//		} else if (direction == "left") {
+//			leftGroup.GetComponent<PartyGroup> ().highlight(Color.cyan);
+//			rightGroup.GetComponent<PartyGroup> ().highlight(Color.white);
+//		} else {
+//
+//			leftGroup.GetComponent<PartyGroup> ().highlight(Color.white);
+//			rightGroup.GetComponent<PartyGroup> ().highlight(Color.white);
+//		}
 	}
 
 	public void hightlight(string direction, Color c) {
-		if (direction == "right") {
-			rightGroup.GetComponent<PartyGroup> ().highlight (c);
-		} else if (direction == "left") {
-			leftGroup.GetComponent<PartyGroup> ().highlight(c);
-		} else {
-			leftGroup.GetComponent<PartyGroup> ().highlight(c);
-			rightGroup.GetComponent<PartyGroup> ().highlight (c);
+//		if (direction == "right") {
+//			rightGroup.GetComponent<PartyGroup> ().highlight (c);
+//		} else if (direction == "left") {
+//			leftGroup.GetComponent<PartyGroup> ().highlight(c);
+//		} else {
+//			leftGroup.GetComponent<PartyGroup> ().highlight(c);
+//			rightGroup.GetComponent<PartyGroup> ().highlight (c);
+//		}
+	}
+
+//	public bool check(string s) {
+//		Debug.Log (s + " = " + side); 
+//		if (s != side && s != "none") {
+//			hightlight (s, Color.green);
+//			return true;
+//		} else {
+//			hightlight (s, Color.red);
+//			return false;
+//		}
+//	}
+
+	public void cool(string side) {
+		if (side == "left") {
+			leftGroup.transform.FindChild ("Emoji").gameObject.GetComponent<Animator> ().SetTrigger ("OhYes");
+		} else if (side == "right") {
+			rightGroup.transform.FindChild ("Emoji").gameObject.GetComponent<Animator> ().SetTrigger ("OhYes");
 		}
 	}
 
-	public bool check(string s) {
-		Debug.Log (s + " = " + side); 
-		if (s != side && s != "none") {
-			hightlight (s, Color.green);
-			return true;
-		} else {
-			hightlight (s, Color.red);
-			return false;
+	public void fail(string side) {
+		if (side == "left") {
+			leftGroup.transform.FindChild ("Emoji").gameObject.GetComponent<Animator> ().SetTrigger ("OhNo");
+		} else if (side == "right") {
+			rightGroup.transform.FindChild ("Emoji").gameObject.GetComponent<Animator> ().SetTrigger ("OhNo");
 		}
 	}
 
-	public void activate() {
-		Debug.Log ("activate");
-		partyGroup.Rotate ();
+	public void activate(string side) {
+		if (side == "left") {
+			leftGroup.GetComponent<PartyGroup> ().Rotate ();
+		} else if (side == "right") {
+			rightGroup.GetComponent<PartyGroup> ().Rotate ();
+		}
+//		Debug.Log ("activate");
+//		partyGroup.Rotate ();
 	}
 		
 }
