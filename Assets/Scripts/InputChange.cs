12c69e8c414b3776c6d9f165ddd62404d3dbc1b7
bigredwill <bigredwill@gmail.com>
2016-01-31 16:51:14 -0800

    Name input stuff

---
+++ Assets/Scripts/InputChange.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class InputChange : MonoBehaviour {
+	Text datext;
+	char[] abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*{}".ToCharArray();
+	int location = 0;
+
+	bool canUpdate = false;
+
+	float nextUpdate;
+
+	// Use this for initialization
+	void Start () {
+		datext = transform.FindChild ("Text").GetComponent<Text> ();
+		Debug.Log (abc[location]);
+		setText (abc[location]);
+
+		nextUpdate = Time.fixedTime;
+
+
+	}
+
+	void OnGUI() { 
+		
+		if (EventSystem.current.currentSelectedGameObject.name == this.name) {
+			canUpdate = true;
+
+		} else {
+			canUpdate = false;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (canUpdate) {
+			float axis = Input.GetAxis ("Vertical");
+			if (Time.fixedTime > nextUpdate) {
+				if (axis == -1) {
+					changeText ("down");
+					nextUpdate = Time.fixedTime + .05f;
+
+				} else if (axis == 1) {
+					changeText ("up");
+					nextUpdate = Time.fixedTime + .05f;
+				}
+			} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				changeText ("down");
+			} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				changeText ("up");
+			}
+		}
+
+
+
+	}
+
+	void changeText(string direction) {
+		if (direction == "down") {
+			location++;
+			if (location >= abc.Length)
+				location = 0;
+			setText (abc [location]);
+		} else if (direction == "up") {
+			location--;
+			if (location < 0)
+				location = abc.Length -1;
+			setText (abc [location]);
+		}
+	}
+
+	void setText(char c) {
+		
+		datext.text = c.ToString();
+
+	}
+
+
+	
+
+}
---
+++ Assets/Scripts/NameInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour {
+
+
+	public GameObject input1;
+	public GameObject input2;
+	public GameObject input3;
+
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+//		GUI.SetNextControlName("Input1");
+		GUI.FocusControl ("Input1");
+		name = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void submitSelected() {
+		string in1 = input1.transform.FindChild ("Text").GetComponent<Text> ().text;
+		string in2 = input2.transform.FindChild ("Text").GetComponent<Text> ().text;
+		string in3 = input2.transform.FindChild ("Text").GetComponent<Text> ().text;
+
+		name = in1 + in2 + in3;
+		Debug.Log (name);
+
+//		
+	}
+
+
+}
